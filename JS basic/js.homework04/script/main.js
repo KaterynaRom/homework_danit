751fa87b2ae25a1ca523d9a68a804c91b07532ed
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-07-08 23:18:16 +0300

    HW04+

--- JS basic/js.homework04/script/main.js
+++ JS basic/js.homework04/script/main.js
@@ -1,16 +1,15 @@
-
 function createNewUser ( ){
     let name = prompt('Enter your name.');
     let surname = prompt('Enter your last name.');
     let result = '';
-        return {
-            firstName: name,
-            lastName: surname,
-            getDiscount: function(){
-                result += this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
-                return result;
-            }
+    return {
+        firstName: name,
+        lastName: surname,
+        getDiscount: function(){
+            result += this.firstName[0].toLowerCase() + this.lastName.toLowerCase();
+            return result;
         }
+    }
  }
 const newUser = createNewUser();
 console.log(newUser.getDiscount());
