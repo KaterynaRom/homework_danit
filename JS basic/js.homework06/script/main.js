54a935b469037795339d1d8d626fb05f0cd2ad81
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-07-19 13:07:34 +0300

    HW06

---
+++ JS basic/js.homework06/script/main.js
@@ -0,0 +1,22 @@
+
+let arr = [15, '25', 'hallow', 2, '5', 'seven', null];
+const type = 'number';
+let filterBy = arr.filter(item => typeof item !== type);
+
+console.log(filterBy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
