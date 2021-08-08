bdaa720a585398af7b4b8f9bf576000ad02affdc
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-08-09 00:31:48 +0300

    HW10.all

--- JS basic/js.homework10/script/main.js
+++ JS basic/js.homework10/script/main.js
@@ -1,63 +1,55 @@
-//
-//
-// const inputPassword = document.querySelector('.password');
-// const inputShowPassword = document.querySelector('#password');
-//
-// inputShowPassword.addEventListener('click', (e) => {
-//     if (inputPassword.type === 'text') {
-//         inputPassword.type = "password";
-//         inputShowPassword.classList.add('fa-eye-slash');
-//         inputShowPassword.classList.remove('fa-eye');
-//     } else {
-//         inputPassword.type = 'text';
-//         inputShowPassword.classList.add('fa-eye');
-//         inputShowPassword.classList.remove('fa-eye-slash');
-//     }
-//
-// })
-//
-//
-//
-// const inputPasswordRepeat = document.querySelector('.passwordRepeat');
-// const inputShowPasswordRepeat = document.querySelector('#passwordRepeat');
-//
-//
-// inputShowPasswordRepeat.addEventListener('click', (e) => {
-//     if (inputPasswordRepeat.type === 'text') {
-//         inputPasswordRepeat.type = "password";
-//         inputShowPasswordRepeat.classList.add('fa-eye-slash');
-//         inputShowPasswordRepeat.classList.remove('fa-eye');
-//     } else {
-//         inputPasswordRepeat.type = 'text';
-//         inputShowPasswordRepeat.classList.add('fa-eye');
-//         inputShowPasswordRepeat.classList.remove('fa-eye-slash');
-//     }
-//
-// })
 
+const inputPassword = document.querySelector('.password');
+const inputShowPassword = document.querySelector('#password');
 
-// попытка
-
-const icons = document.querySelector('.password-form');
-const inputPassword = document.querySelectorAll('.passwordAll');
-
-icons.addEventListener('click', (event) => {
-    // let selectedInput = event.target.dataset.password;
-    let selectedIcon = event.target.dataset.icon;
-
+inputShowPassword.addEventListener('click', (e) => {
     if (inputPassword.type === 'text') {
         inputPassword.type = "password";
-        selectedIcon.classList.add('fa-eye-slash');
-        selectedIcon.classList.remove('fa-eye');
+        inputShowPassword.classList.add('fa-eye-slash');
+        inputShowPassword.classList.remove('fa-eye');
     } else {
         inputPassword.type = 'text';
-        selectedIcon.classList.add('fa-eye');
-        selectedIcon.classList.remove('fa-eye-slash');
+        inputShowPassword.classList.add('fa-eye');
+        inputShowPassword.classList.remove('fa-eye-slash');
+    }
+
+});
+
+const inputPasswordRepeat = document.querySelector('.passwordRepeat');
+const inputShowPasswordRepeat = document.querySelector('#passwordRepeat');
+
+inputShowPasswordRepeat.addEventListener('click', (e) => {
+    if (inputPasswordRepeat.type === 'text') {
+        inputPasswordRepeat.type = "password";
+        inputShowPasswordRepeat.classList.add('fa-eye-slash');
+        inputShowPasswordRepeat.classList.remove('fa-eye');
+    } else {
+        inputPasswordRepeat.type = 'text';
+        inputShowPasswordRepeat.classList.add('fa-eye');
+        inputShowPasswordRepeat.classList.remove('fa-eye-slash');
     }
 });
 
+const button = document.querySelector('.btn');
+const error = document.querySelector('.error');
 
 
+button.addEventListener('click', () => {
+    const password = inputPassword.value;
+    const passwordRepead = inputPasswordRepeat.value;
+
+    if (password === passwordRepead){
+        alert('You are welcome');
+    } else {
+        error.classList.remove('error');
+        error.classList.add('errorShow');
+    }
+});
+
+button.addEventListener('blur', () => {
+    error.classList.remove('errorShow');
+    error.classList.add('error');
+});
 
 
 
