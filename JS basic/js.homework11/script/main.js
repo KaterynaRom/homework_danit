33ad530387244f37ad20af32b614dafed1bb859c
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-08-09 17:15:07 +0300

    HW11/03

--- JS basic/js.homework11/script/main.js
+++ JS basic/js.homework11/script/main.js
@@ -8,7 +8,10 @@ buttonList.addEventListener('keyup', (e) =>{
         elem.classList.remove('active');
         elem.classList.add('standartBg');
         let selectedEl = e.code;
-        if (elem.innerText === selectedEl.slice(3)) {
+        if (selectedEl === elem.innerText){
+            elem.classList.add('active');
+            elem.classList.remove('standartBg');
+        } else if (elem.innerText === selectedEl.slice(3)) {
             elem.classList.add('active');
             elem.classList.remove('standartBg');
             console.log(selectedEl);
