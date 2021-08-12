36408d6770cb09d32181d6370ffcb4989a13cbaf
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-08-13 01:37:07 +0300

    +script1

---
+++ Project/script/main.js
@@ -0,0 +1,29 @@
+// ***************** # 1 ***********
+
+let selectedTab = 'web';
+
+const servicesTabs = document.querySelector('.services-tabs');
+
+servicesTabs.addEventListener('click', (event) => {
+    selectedTab = event.target.dataset.category;
+    const items = document.querySelectorAll('.services-content-item');
+    items.forEach(elem => {
+        elem.classList.remove('visible');
+
+        if (elem.dataset.category === selectedTab) {
+            elem.classList.add('visible');
+        }
+    });
+
+    const tabs = document.querySelectorAll('.services-tabs-title');
+    tabs.forEach(elem => {
+        elem.classList.remove('active');
+
+        if (elem.dataset.category === selectedTab) {
+            elem.classList.add('active');
+        }
+    });
+
+});
+
+// ***************** # 2 ***********
