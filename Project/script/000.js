3cfbf11bd23545f9770a13e531177bc9fdb83834
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-08-18 02:33:45 +0300

    poj+

--- Project/script/000.js
+++ Project/script/000.js
@@ -115,65 +115,59 @@ const images = [
         category: 'Wordpress',
     },
 ];
-
-
-const workContainer = document.querySelector('.work-container');
-const workTabs = document.querySelector('.work-tabs');
 let perPage = 12;
-let category = 'All';
-
-const content = (arr) =>{
-    return arr.map(elem => {
-        return `
-    <img class="work-item" src="${elem.src}" alt="">
-    <!--<div></div>-->
-    `;
-    }).slice(0, perPage).join(' ');
-};
+let currentCategory=`All`;
 
 
-const filterArr = (arr, category) => {
+
+const render = (arr) => {
+    const mainContainer = document.querySelector('.work-container');
+    const sliceArray = arr.slice(0, perPage);
+    const htmlArray = sliceArray.map((element) => {
+        return `<img class="work-item" src="${element.src}">`
+    });
+    mainContainer.innerHTML = htmlArray.join(' ');
+    const btn = document.querySelector('.button-load');
+
+    if (perPage >= arr.length) {
+        btn.classList.remove(`invisible`);
+    } else {
+        btn.classList.add(`invisible`);
+    }
+}
+
+const filteredArr = (arr, category) => {
     return arr.filter((e)=> e.category === category)
 }
 
-workContainer.innerHTML = content(images);
 
-workTabs.addEventListener('click', e => {
-    selectedTab = e.target.dataset.work;
-    category =  selectedTab;
-    if (selectedTab !== 'All') {
-        workContainer.innerHTML = content(filterArr(images, category));
-        // content(filterArr(images, category);
-    } else {
-        workContainer.innerHTML = content(images);
-        // content(images);
-    }
+console.log(filteredArr(images))
 
+const tabsContainer = document.querySelector('.work-tabs');
+
+tabsContainer.addEventListener('click', (event) => {
+    const category = event.target.dataset.work;
+    currentCategory = category;
+    perPage = 12;
+    if (category === "all" && event.target !== event.currentTarget) {
+        render(images);
+    } else if (event.target !== event.currentTarget) {
+        const newArr = filteredArr(images, category);
+        render(newArr);}
     const tabs = document.querySelectorAll('.work-tabs-title');
-    tabs.forEach(elem => {
-        elem.classList.remove('active');
-
-        if (elem.dataset.work === selectedTab) {
-            elem.classList.add('active');
-        }
+    tabs.forEach(e => {
+        e.classList.remove('active');
     });
+    event.target.classList.add('active');
 });
 
-const button = document.querySelector('.button-load');
-
-button.addEventListener('click', e => {
-    // const category = e.target.dataset.work;
+const button = document.querySelector(".button-load");
+button.addEventListener(`click`, () => {
     perPage = perPage+12;
-    if(selectedTab===`All`){
-        content(images);
-    } else{
-        content(filterArr(images, category));
+    if(currentCategory===`All`){
+        render(images);
+    }else{
+        render(filteredArr (images, currentCategory));
     }
-
-    if(perPage === 36) {
-        button.classList.add('invisible');
-    } else {
-        button.classList.remove('invisible');
-    }
-
 })
+
