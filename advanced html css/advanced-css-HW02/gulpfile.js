6739f255603c674aceb597778f03fa2c4982737f
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-09-06 23:31:00 +0300

    HW02adv

--- advanced html css/advanced-css-HW02/gulpfile.js
+++ advanced html css/advanced-css-HW02/gulpfile.js
@@ -7,6 +7,7 @@ import autoprefixer from "gulp-autoprefixer";
 import clean from "gulp-clean";
 import imagemin from "gulp-imagemin";
 import sass from "gulp-sass";
+import purgecss from "gulp-purgecss";
 
 import BS from 'browser-sync';
 const browserSync = BS.create();
@@ -16,10 +17,14 @@ const browserSync = BS.create();
 
 const buildCss = () =>  gulp.src("src/scss/*.scss").
 pipe(sass().on('error', sass.logError)).
-pipe(concat("styles.min.css")).
-pipe(autoprefixer({
-   cascade: false
+pipe(purgecss({
+  content: ['src/**/*.html']
 })).
+pipe(concat("styles.min.css")).
+// pipe(autoprefixer({
+//     browsers: ['last 3 versions'],
+//     cascade: false
+// })).
 pipe(cleanCSS({compatibility: 'ie8'})).
 pipe(gulp.dest("dist/css"));
 
@@ -29,16 +34,14 @@ pipe(concat("scripts.min.js")).
 pipe(minify()).
 pipe(gulp.dest("dist/js"));
 
-
 const cleanDist = () => gulp.src("dist/*", {read: false}).pipe(clean());
 
 const minifyImages = () => gulp.src("src/img/*").pipe(imagemin()).pipe(gulp.dest('dist/img'));
 
-
 gulp.task("buildCss", buildCss);
 gulp.task("buildJs", buildJS);
 gulp.task("minifyImages", minifyImages);
-gulp.task('build', gulp.series("buildCss", "buildJs", 'minifyImages'));
+// gulp.task('build', gulp.series("buildCss", "buildJs", 'minifyImages'));
 gulp.task("cleanDist", cleanDist);
 
 
@@ -52,4 +55,4 @@ const dist = () => {
 }
 
 gulp.task('dev', dist);
-gulp.task('build', build());
+// gulp.task('build', build());
