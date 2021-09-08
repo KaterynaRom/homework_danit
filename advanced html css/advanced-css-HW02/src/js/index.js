aea45edc70db99bfe245e4a587b13bf6f856cf54
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-09-08 21:57:57 +0300

    HW02 /+

--- advanced html css/advanced-css-HW02/src/js/index.js
+++ advanced html css/advanced-css-HW02/src/js/index.js
@@ -2,9 +2,11 @@ const button = document.querySelector('.header__button');
 const menu = document.querySelector('.header__nav');
 
 button.addEventListener('click', () => {
-    menu.classList.toggle('.header__nav--active');
+    menu.classList.toggle('header__nav--active');
     const active = document.querySelector('.header__nav--active');
     if (active) {
         button.innerHTML = `<img src="dist/img/menu-button-open.svg" alt="">`;
+    } else {
+        button.innerHTML = `<img src="dist/img/menu-button-closed.svg" alt="">`;
     }
 });
