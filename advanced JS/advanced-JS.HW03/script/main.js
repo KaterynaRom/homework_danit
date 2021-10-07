749b4b9e033557db723f48aa5b5583f977a70853
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-10-07 17:55:21 +0300

    advJS HW03+

--- advanced JS/advanced-JS.HW03/script/main.js
+++ advanced JS/advanced-JS.HW03/script/main.js
@@ -1,7 +1,7 @@
 // Задание 1
 const clients1 = ["Гилберт", "Сальваторе", "Пирс", "Соммерс", "Форбс", "Донован", "Беннет"];
 const clients2 = ["Пирс", "Зальцман", "Сальваторе", "Майклсон"];
-const clients = new Set ([...clients1, ...clients2]);
+const clients = [...new Set ([...clients1, ...clients2])];
 console.log(clients);
 
 
