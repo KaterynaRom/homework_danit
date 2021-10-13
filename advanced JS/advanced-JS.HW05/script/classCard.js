58c10575041ac03b14bef5ae55133bb912efb2bb
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-10-13 18:05:38 +0300

    HW05 JS adv

---
+++ advanced JS/advanced-JS.HW05/script/classCard.js
@@ -0,0 +1,38 @@
+export class Card {
+  constructor(name, email, title, text, id, postId) {
+    this.postId = postId;
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.title = title;
+    this.text = text;
+    this.card = document.createElement('div');
+    this.button = document.createElement('button');
+  }
+  render (){
+    this.card.classList.add('card');
+    this.card.id = `${this.id}`;
+    this.card.append(this.button);
+    this.button.classList.add('delete');
+    this.button.innerHTML = 'X';
+    this.button.addEventListener('click', this.deleteCard.bind(this));
+    this.card.insertAdjacentHTML('beforeend', `
+        <p>${this.name}</p>
+        <p>${this.email}</p>
+        <h3>${this.title}</h3>
+        <p>${this.text}</p>
+      `);
+    return this.card;
+  }
+
+  deleteCard (){
+  
+    this.button.addEventListener('click', e => {
+      axios.delete(`https://ajax.test-danit.com/api/json/posts/${this.postId}`, {
+
+
+      })
+    })
+  }
+
+}
---
+++ advanced JS/advanced-JS.HW05/script/main.js
@@ -0,0 +1,34 @@
+import {Card} from "./classCard.js";
+const urlUsers = 'https://ajax.test-danit.com/api/json/users';
+const urlPosts = 'https://ajax.test-danit.com/api/json/posts';
+const container = document.querySelector('.container');
+
+let users = {};
+
+axios.get(urlUsers).then(({ data, status }) => {
+  if(status === 200) {
+    return  data;
+  } else {
+    console.error('Bad response')
+  }
+}).then(usersList => {
+  usersList.forEach(e => {
+    users[`id-${e.id}`] = e;
+    })
+  }).then(
+  axios.get(urlPosts).then(({ data, status }) => {
+    if(status === 200){
+      data.forEach(({ userId: id, title, body: text, id: postId }) => {
+        const email = users[`id-${id}`].email;
+        const name = users[`id-${id}`].name;
+        container.append(new Card(title, text, name, email, id, postId).render());
+      })
+    } else {
+      console.error('Bad response')
+    }
+  })
+).catch(err => console.log(err))
+
+console.log(users)
+
+
