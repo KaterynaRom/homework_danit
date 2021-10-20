e18ade756f63d0a07fb8a60c2545316d58d9e143
Kateryna Romanovska <romanovska.katya.s@gmail.com>
2021-10-20 14:45:13 +0300

    HW06/IP

--- advanced JS/advanced-JS.HW06/script/main.js
+++ advanced JS/advanced-JS.HW06/script/main.js
@@ -1,12 +1,3 @@
-//
-// Создать простую HTML страницу с кнопкой Вычислить по IP.
-//   По нажатию на кнопку - отправить AJAX запрос по адресу
-// https://api.ipify.org/?format=json, получить оттуда IP адрес клиента.
-//   Узнав IP адрес, отправить запрос на сервис
-// https://ip-api.com/ и получить информацию о физическом адресе.
-//   Под кнопкой вывести на страницу информацию, полученную из
-// последнего запроса - континент, страна, регион, город, район города.
-//   Все запросы на сервер необходимо выполнить с помощью async await.
 
 const urlIp = 'https://api.ipify.org/?format=json';
 const urlUser = 'http://ip-api.com/json/';
@@ -16,30 +7,17 @@ const container = document.querySelector('.container');
 button.addEventListener('click', getInfo);
 
 async function getInfo () {
-  await fetch(urlIp).then(res => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      console.error('Bad response')
-    }
-  }).then(({ip}) => {
-    fetch(urlUser+ip).then(res => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        console.error('Bad response')
-      }
-    }).then(({ country, regionName, region, city, zip }) => {
-      container.insertAdjacentHTML('beforeend', `
+  try {
+    const myIp = await fetch(urlIp).then(res => res.json()).then(({ip}) => ip);
+    const { country, regionName, region, city, zip } = await fetch(urlUser + myIp).then(res => res.json());
+    container.insertAdjacentHTML('beforeend', `
         <p>country: ${country}</p>
         <p>region name: ${regionName}</p>
         <p>region: ${region}</p>
         <p>city: ${city}</p>
-        <p>zip: ${zip}</p>
-      `)
-    })
-  })
+        <p>zip: ${zip}</p>`);
+  } catch (err) {
+    console.log(err)
+  }
 }
-
-
 
